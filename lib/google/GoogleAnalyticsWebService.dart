cd72aed4065cea95c35cf719d4483704aece4ae1
Mark Eggenstein <>
2021-02-26 13:34:48 +0100

    Fixed depencdency on Firebase base package.

--- lib/google/GoogleAnalyticsWebService.dart
+++ lib/google/GoogleAnalyticsWebService.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:eggnstone_flutter/eggnstone_flutter.dart';
 import 'package:eggnstone_google_analytics/google/IGoogleAnalyticsService.dart';
-import 'package:firebase/firebase.dart' as FirebaseWeb;
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 
 /// Requires [LoggerService]
 class GoogleAnalyticsService
@@ -15,7 +15,7 @@ class GoogleAnalyticsService
     static const int MAX_PARAM_NAME_LENGTH = 40;
     static const int MAX_PARAM_VALUE_LENGTH = 100;
 
-    final FirebaseWeb.Analytics _firebaseAnalytics;
+    final FirebaseAnalyticsWeb _firebaseAnalytics;
 
     bool _isEnabled;
     String _currentScreen;
@@ -28,10 +28,10 @@ class GoogleAnalyticsService
 
     /// Requires [LoggerService]
     static Future<IGoogleAnalyticsService> create(bool startEnabled)
-    => GoogleAnalyticsService.createMockable(FirebaseWeb.analytics(), startEnabled);
+    => GoogleAnalyticsService.createMockable(FirebaseAnalyticsWeb(), startEnabled);
 
     /// Requires [LoggerService]
-    static Future<IGoogleAnalyticsService> createMockable(FirebaseWeb.Analytics firebaseWebAnalytics, bool startEnabled)
+    static Future<IGoogleAnalyticsService> createMockable(FirebaseAnalyticsWeb firebaseWebAnalytics, bool startEnabled)
     async
     {
         var instance = GoogleAnalyticsService._internal(firebaseWebAnalytics, startEnabled);
@@ -62,7 +62,7 @@ class GoogleAnalyticsService
         _currentScreen = newValue;
 
         if (_isEnabled)
-            _firebaseAnalytics.setCurrentScreen(newValue);
+            _firebaseAnalytics.setCurrentScreen(screenName: newValue);
     }
 
     @override
@@ -115,7 +115,7 @@ class GoogleAnalyticsService
         logger.logInfo(s);
 
         if (_isEnabled)
-            _firebaseAnalytics.logEvent(name, safeParams);
+            _firebaseAnalytics.logEvent(name: name, parameters: safeParams);
     }
 
     @override
