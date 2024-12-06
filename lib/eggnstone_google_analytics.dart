c383264acbdfb2a5bf4544be3881c092c0ebd5d3
Mark Eggenstein <mark.eggenstein@eggnstone.com>
2024-12-06 14:25:25 +0700

    Updated lint rules.

--- lib/eggnstone_google_analytics.dart
+++ lib/eggnstone_google_analytics.dart
@@ -1,5 +1,5 @@
 /// eggnstone_google_analytics
-library eggnstone_google_analytics;
+library;
 
 export 'src/GoogleAnalyticsNonWebService.dart' if (dart.library.html) 'src/GoogleAnalyticsWebService.dart';
 export 'src/IGoogleAnalyticsService.dart';
